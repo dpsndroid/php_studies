09df1f9e31ebc625c479c220db85b4e39dd10aee
Daniel Nepomuceno <105947140+dpsndroid@users.noreply.github.com>
2022-08-08 22:13:27 -0300

    Update_aug08

--- phpoo/ex001.php
+++ phpoo/ex001.php
@@ -8,6 +8,26 @@
 </head>
 <body>
     <h1>PHP orientado a objetos</h1>
-    
+    <?php
+        require_once "Caneta.php";
+        $c1 = new Caneta;
+        $c1->cor = "Azul";
+        $c1->ponta = 0.5;
+        $c1->tampada = false;
+        $c1->destampar();
+#----------------------------------------------------------------       
+        $c1->rabiscar();    # chama o método rabiscar da classe
+        echo "<br><br>";
+        var_dump($c1);
+        echo "<br><br>";
+        print_r($c1);
+#-------------------------------------------------------------
+        $c2 = new Caneta;
+        $c2->cor = "verde";
+        $c2->carga = 50;
+        $c2->tampar();
+        echo "<br>";
+        print_r($c2);
+    ?>
 </body>
 </html>
